a4348921ad7128e8a0f89ff4207134b90a9ba355
rajneeshverma1 <curiousrajneesh2024@gmail.com>
2026-04-13 23:25:21 +0530

    Day 2.1: Add variables, data types, and if/else control structures

--- internal/app/day2_basics.go
+++ internal/app/day2_basics.go
@@ -130,20 +130,3 @@ func Day2Basics() string {
 
 	return result
 }
-
-// add returns the sum of two integers
-func add(a int, b int) int {
-	return a + b
-}
-
-// multiply returns the product of two integers
-func multiply(a int, b int) int {
-	return a * b
-}
-
-// divide returns quotient and remainder
-func divide(a int, b int) (int, int) {
-	quotient := a / b
-	remainder := a % b
-	return quotient, remainder
-}
